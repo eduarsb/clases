db73fb15111be5465d3f0eff3455b0abdcf43d42
eduarsb <eduarsb4@gmail.com>
2017-01-23 03:57:17 -0600

    Resolución de ejercicios
    Ejercicios resueltos en su mayoría

--- Ejercicios/01_arreglos.php
+++ Ejercicios/01_arreglos.php
@@ -32,28 +32,100 @@ $data['consolas'] = array(
 
 $data['moviles'] = array(
 	'android' => array('pokemon go', 'flow', 'flappy bird'),
-	'ios' => array('mario run', 'monsters fo sea', 'jet pack')
+	'ios' => array('mario run', 'monsters fo sea', 'jet pack', ' x juego')
 );
+
 
 print_r($data);
 
+
 echo "<br><br>";
 echo "Con base en el arreglo anterior obtner lo siguiente: <br>";
+
 echo "Mostrar la empresa 'Sony' <br>";
+echo $data['consolas'][0]['empresa'];
+
 echo "Mostrar el precio de la Xbox one <br>";
+echo $data['consolas'][1]['precio'];
+
 echo "Mostrar la consola 'Nintendo switch' <br>";
+echo $data['consolas'][2]['nombre'];
+
 echo "Mostrar el videojuego de 'uncharted' <br>";
+echo $data['consolas'][0]['videojuegos']['plataforms'][2];
+
 echo "Mostrar el videojuego de 'gears of war' <br>";
 echo "Mostrar el videojuego de 'Mario kart' <br>";
 echo "Mostrar el videojuego de 'flappy bird";
 echo "Mostrar el videojuego de 'mario run";
+
+
 echo "Listar el nombre de las consolas";
+foreach ($data['consolas'] as $value) {
+	print_r($value['nombre']);
+	echo "<br>";
+
+}
+
+
 echo "Listar los de juegos de plataforms de Playstation 4 <br>";
+foreach ($data['consolas'][0]['videojuegos']['plataforms'] as $value) {
+	print_r($value);
+	echo "<br>";
+
+}
+
+
 echo "Listar los de juegos de shooters de Xbox one <br>";
 echo "Listar los de juegos de Nintendo switch <br>";
-echo "Listar todos los juegos moviles, tanto los de android como los de ios <br>";
-echo "Listar todos los juegos de Playstation 4 (usando maximo solo 2 foreach) <br>";
-echo "Mostrar el precio total de las consolas <br>";
 
+
+echo "Listar todos los juegos moviles, tanto los de android como los de ios <br>";
+//Forma 1
+foreach ($data['moviles'] as $value) {
+	//print_r($value);
+	foreach ($value as $videojuego) {
+		echo $i. " videojuego :";
+		print_r($videojuego);
+		echo "<br>";
+		$i++;
+	}
+}
+
+//Forma 2
+for ($j=0; $j < count($data['moviles']['android']); $j++) { 
+	echo $data['moviles']['android'][$j];
+	echo '<br><br>';
+}
+
+for ($j=0; $j < count($data['moviles']['ios']); $j++) { 
+	echo $data['moviles']['ios'][$j];
+	echo '<br><br>';
+}
+
+
+
+echo "Listar todos los juegos de Playstation 4 (usando maximo solo 2 foreach) <br>";
+foreach ($data['consolas'][0]['videojuegos'] as $value) {
+	foreach ($value as $val) {
+		print_r($val);
+		echo "<br>";
+	}
+}
+
+
+echo "Mostrar el precio total de las consolas <br>";
+//Froma 1
+$total = $data['consolas'][0]['precio'] + $data['consolas'][1]['precio'] + $data['consolas'][2]['precio'];
+echo $total;
+
+
+//Forma 2
+$total = 0;
+foreach ($data['consolas'] as $value) {
+	$total = $value['precio'] + $total;
+	echo "<br><br>";
+}
+echo $total;
 
 ?>
